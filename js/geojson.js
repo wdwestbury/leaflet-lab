a91bd2b5e16493e3373247864f9c143e8a04a18c
wdwestbury <wdwestbury@gmail.com>
2016-02-25 11:41:41 -0600

    module 5 completed
    module 5 has been completed. geojson is broken down into separate files
    for each year to reduce size, may need to reformat depending on how
    slider works.

--- js/geojson.js
+++ js/geojson.js
@@ -1,5 +1,5 @@
 // initialize the map with geographical coordinates set on madison
-var map = L.map("map").setView([43, -89], 2);
+var map = L.map("map").setView([40, -101], 4);
 
 //	add a tile layer to the map
 var cartoDB_Map = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', 
@@ -9,10 +9,90 @@ var cartoDB_Map = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x
 	maxZoom: 19
 }).addTo(map);
 
+
+
 // call getdata
 getData(map);
 
-// add the data in megacities.geojson to the map
+
+
+// create some proportional symbols
+function createPropSymbols(data,map)
+{
+	// use pop_2015 to create proportional symbols
+	var attribute = "construction";
+
+	// create markers with cool options
+	var geojsonMarkers = 
+	{
+		radius: 8,
+		fillColor: "#c3dbfe",
+		color: "#6fa9fe",
+		weight: 1,
+		opacity: 1,
+		fillOpacity: 0.8
+	};
+
+	// creat a geojson layer and add it to the map
+	L.geoJson(data,
+	{
+		pointToLayer: function (feature, latlng)
+		{
+			// determine attribute value for each feature
+			var attributeValue = Number(feature.properties[attribute]);
+
+			// log the attribute value in the console
+			console.log(feature.properties, attributeValue);
+
+			// determine radius for markers based on attribute value
+			geojsonMarkers.radius = calculatePropRadius(attributeValue);
+
+			//create the circle markers
+			return L.circleMarker(latlng,geojsonMarkers);
+		}
+	}).addTo(map);
+};
+
+
+
+// calculate the radius of the proportional symbol
+function calculatePropRadius(attributeValue)
+{
+	// set the scale factor
+	var scaleFactor = 50;
+
+	// set area based on the attribute value and the scale factor
+	var area = attributeValue * scaleFactor;
+
+	// calculate radius using area
+	var radius = Math.sqrt(area/Math.PI);
+
+	return radius;
+}
+
+
+
+// get some data and put it in the map
+function getData(map)
+{
+	// get the data from the geojson file
+	$.ajax("data/industry2008.geojson",
+	{
+		dataType: "json",
+		success: function(response)
+		{
+			// call function to create proportional symbols
+			createPropSymbols(response, map);
+		}
+	});
+};
+
+
+
+
+// code from 2/17/16
+/*
+//add the data in megacities.geojson to the map
 function getData(map)
 {
 	$.ajax("data/MegaCities.geojson",
@@ -21,7 +101,8 @@ function getData(map)
 		success: function(response)
 		{
 			// create marker with cool options
-			var cityMarkers = {
+			var cityMarkers = 
+			{
 				radius: 8,
 				fillColor: "#c3dbfe",
 				color: "#6fa9fe",
@@ -41,3 +122,4 @@ function getData(map)
 		}
 	});	
 };
+*/
